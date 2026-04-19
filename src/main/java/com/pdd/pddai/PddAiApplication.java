ad26ad8e35c1f424b240736487d9be08dac4b23a
gudvin22 <64dro@rambler.ru>
2026-04-19 19:36:42 +0700

    init commit

---
+++ src/main/java/com/pdd/pddai/PddAiApplication.java
@@ -0,0 +1,13 @@
+package com.pdd.pddai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PddAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PddAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pdd/pddai/PddAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdd.pddai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PddAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
